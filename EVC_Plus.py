2b32c2d4d6826762f4f1a51c961edd1a86a4f540
Isak Developer <mrisak192@gmail.com>
2022-11-09 12:57:43 +0300

    Version 1.0.0

--- EVC_Plus.py
+++ EVC_Plus.py
@@ -1,83 +1,99 @@
-pin=1234
-password=int(input("Fadlan Gali Pin-kaaga"))
-if password == pin:
-    print("[-EVC PLUS-]")
-    print("1. Itus Haraaga")
-    print("2. Kaarka hadalka")
-    print("3. Bixi Bill")
-    print("4. Uwareeji EVC Plus")
-    #CHOOSE OPTION
-    Option=int(input("Fadlan Option dooro"))   
-    #START OPTION ONE
-    if Option==1:
-        print("[-EVC PLUS-]")
-        print("Haraagagu waa 100000$")
-     #START OPTION TWO
-    elif Option==2:
-        print("KAARKA HADALKA")
-        print("1. Ku shubo Airtime")
-        print("2. Ugu shub Airtime")
-        kaar=int(input("Falan dooro"))
-        if  kaar==1:
-            money=int(input("Fadlan Gali Lacagta"))
-            print("Waxaad Ku shubatay",money,"$")
-        elif kaar==2:
-            mobile=int(input("Falan Gali mobileka"))
-            money=int(input("Fadlan gali Lacagta"))
-            print("ma hubtaa in aad",money,"$ ugu shubto",mobile)
-            print("1. Haa")
-            print("2. Maya")
-            hubin=int(input("Fadlan dooro"))
-            if hubin==1:
-                print("$",money,"ayaad u wareejisay",mobile,"Tar:03/11/22 12:00:00 Haraagagu waa 1000$.")
-            else:
-                print("waa ka labatay ")    
-        else:
-            print("Macsalaamo")
+Balance=1000
 
-     #START OPTION THREE    
-    elif Option==3:
-        print("3. Bixi Bill")
+from datetime import datetime
+now=datetime.now()
+current_time =now.strftime("%d-%m-%y \n %H-%M-%S")
 
-     #START OPTION FOUR    
-    elif Option==4:
-        print("[-EVC PLUS-]")
-        mobile=int(input("Falan Gali mobileka"))
-        money=int(input("Fadlan gali Lacagta"))
-        print("Ma hubtaa inaad ",money,"$ u wareejiso ",mobile,"?")
-        print("1. Haa")
-        print("2. Maya")
-        hubin=int(input("Fadlan dooro"))
-        if hubin==1:
-            print("$",money,"ayaad u wareejisay",mobile,"Tar:03/11/22 12:00:00 Haraagagu waa 1000$.")
-        else:
-            print("waa ka labatay")         
-    elif Option==5:
-        print("5. Warbixin Kooban")
-    elif Option==6:
-        print("6. Salaam Bank")
-    elif Option==7:
-        print("7. Maareynta")
-    elif Option==8:
-        print("8. TAAJ")
-    elif Option==9:
-        print("9. Bill Payment")
-    else:
-        print("Macsalaamo")
+CreatePin=int(input("Fadlan Gali PIN-Kaaga Cusub"))
+ConfirmPin=int(input("Fadlan Ku Celi Gali PIN-Kaaga Cusub"))
+
+if CreatePin != ConfirmPin:
+    print("Fadlan Iska Hubi PIN-Kaaaga")
 else:
-    print("Numbarka Sirta aad galisay waa khalad")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    pin=int(input("Fadlan Gali Pinkaaga"))
+    
+    if pin == ConfirmPin:
+        print("[-EVC PLUS-]")
+        print("1. Itus Haraaga")
+        print("2. Kaarka hadalka")
+        print("3. Bixi Bill")
+        print("4. Uwareeji EVC Plus")
+        Option=int(input("Fadlan Option dooro "))
+        if Option == 1:
+            print("[-EVC PLUS-]")
+            print(f"Haraagagu waa {Balance}$")
+        elif Option == 2:
+            print("KAARKA HADALKA")
+            print("1. Ku shubo Airtime")
+            print("2. Ugu shub Airtime")
+            print("3. MIFI Packages")
+            print("4. Ku shubo Internet")
+            print("5. Ugu Shub qof kale (MMT)")
+            Airtime=int(input("Falan dooro"))
+            if Airtime == 1:
+                money=int(input("Fadlan Gali Lacagta "))
+                Balance-=money
+                if Balance < money:
+                    print("Haraagagu Xisaabtaada kuguma filna!")
+                else:
+                    print(f"Waxaad Ku shubatay ${money} Haraagagu waa ${Balance}")
+            elif Airtime== 2:
+                mobile=int(input("Falan Gali mobileka "))
+                money=int(input("Fadlan gali Lacagta "))
+                print(f"ma hubtaa in aad ${money} aad ugu shubto {mobile}?")
+                print(f" 1. Haa \n 2. Maya")
+                hubin=int(input("Fadlan dooro"))
+                if hubin==1:
+                    if Balance < money:
+                        print("Haraagagu Xisaabtaada kuguma filna!")
+                    else:
+                        Balance-=money
+                        print(f"${money} ayaad u wareejisay {mobile}, Taarikhdu {current_time}, Haraagagu waa ${Balance}")
+                elif hubin==2:
+                    print("Waad ka laabatay lacag diridii Mahadsanid")
+                else:
+                    print("MACSALAAMO!!")
+            else:
+                print("MACSALAAMO")
+        elif Option == 3:
+            print("[-EVC PLUS-]")
+        elif Option == 4:
+            print("[-EVC PLUS-]")
+            mobile=int(input("Falan Gali mobileka"))
+            money=int(input("Falan Gali Lacagta"))
+            print(f"Ma hubtaa inaad {money}$ u wareejiso {mobile}?")
+            print("1. Haa \n2. Maya")
+            hubin=int(input("Fadlan dooro"))
+            if hubin == 1:
+                if Balance < money:
+                    print("Haraagagu Xisaabtaada kuguma filna!")
+                else:
+                    Balance-=money
+                    print(f"${money} ayaad u wareejisay {mobile} Taarikhdu {current_time} Haraagagu waa ${Balance}")
+            elif hubin == 2:
+                print("Waad ka laabatay lacag diridii Mahadsanid")
+            else:
+                print("MACSALAAMO")
+            
+        elif Option == 5:
+            print("Warbixin Kooban")
+            print("1. Last Action")
+            print("2. Wareejintii u dambeysay")
+            print("3. Iibsashadii u danbeysay")
+            print("4. Last 3 Actions")
+            print("5. Email Me My Activity")
+        elif Option == 6:
+            print("")
+        elif Option == 7:
+            print("[-EVC PLUS-]")
+        elif Option == 8:
+            print("[-EVC PLUS-]")
+        elif Option == 9:
+            print("[-EVC PLUS-]")
+            
+        else:
+            print("MACSALAAMO")
+    
+    
+    else:
+        print("Numbarka Sirta aad galisay waa khalad")
